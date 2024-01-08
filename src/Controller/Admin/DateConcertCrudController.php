9b37e1b09acb73f1f33605c642358426edbf4463
wendy1976 <caroline.ferru@free.fr>
2024-01-08 11:42:02 +0100

    feat search date_concert in DateConcertCrudController

--- src/Controller/Admin/DateConcertCrudController.php
+++ src/Controller/Admin/DateConcertCrudController.php
@@ -6,6 +6,11 @@ use App\Entity\DateConcert;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
+use Doctrine\ORM\QueryBuilder;
 
 class DateConcertCrudController extends AbstractCrudController
 {
@@ -20,5 +25,21 @@ class DateConcertCrudController extends AbstractCrudController
             IdField::new('id')->onlyOnIndex(),
             DateTimeField::new('date_heure')->setFormat('dd/MM/yyyy à HH:mm'),
         ];
+    }
+
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+{
+    $qb = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
+
+    if ($searchDto->getQuery() !== null) {
+        $searchTerm = \DateTime::createFromFormat('d/m/Y à H:i', $searchDto->getQuery());
+
+        if ($searchTerm !== false) {
+            $qb->andWhere('entity.date_heure = :term')
+               ->setParameter('term', $searchTerm);
+        }
+    }
+
+    return $qb;
     }
 }
